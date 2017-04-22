5b9892bf4b1cb4ad218dd898a6e9a6d37b75582a
seis <xiaonanln@gmail.com>
2017-04-23 01:02:31 +0800

    watchdog works ...

--- src/pyWatchdog/__init__.py
+++ src/pyWatchdog/__init__.py
@@ -1,3 +1,59 @@
+# coding: utf8
+# author: xiaonanln
+
+import sys
+import time
+import traceback
+
+_watchdogs = {}
+_maxWatchdogID = 0
 
 
+class Watchdog(object):
+	def __init__(self, timeout, callback):
+		self.timeout = timeout
+		self.callback = callback
+		self.timeoutTS = time.time() + timeout
 
+	def reset(self):
+		self.timeoutTS = time.time() + self.timeout
+
+	def check(self):
+		if time.time() >= self.timeoutTS:
+			self.reset()
+			self.callback()
+
+def setup(name, timeout, callback):
+	"""Create a watchdog with timeout and callback
+	:arg: timeout timeout of watchdog
+	:arg: callback called when watchdog fail
+	:return: watchdog ID
+	"""
+	_watchdogs[name] = Watchdog(timeout, callback)
+
+
+def cancel(name):
+	if name in _watchdogs:
+		del _watchdogs[name]
+
+
+def reset(name):
+	"""Reset a watchdog so that it won't fail
+	:arg id watchdog ID returned by create function
+	"""
+	assert name in _watchdogs, ('watchdog %s not found' % name)
+	_watchdogs[name].reset()
+
+
+def loop():
+	"""
+	check all watchdogs and make them fail when timeout
+	"""
+	sys.stdout.write('.')
+	for name, watchdog in _watchdogs.iteritems():
+		try:
+			watchdog.check()
+		except:
+			print >>sys.stderr, 'Watchdog %s check failed: ' % name
+			traceback.print_exc()
+			watchdog.reset()
--- tests/testPyWatchdog/test.py
+++ tests/testPyWatchdog/test.py
@@ -1,6 +1,58 @@
 # coding: utf8
 
 import unittest
+import time
+import pyWatchdog
+
+
+class TestPyWatchdog(unittest.TestCase):
+
+	def __init__(self, *args, **kwargs):
+		super(TestPyWatchdog, self).__init__(*args, **kwargs)
+		self.watchdogFailed = False
+
+	def setUp(self):
+		self.watchdogFailed = False
+
+	def tearDown(self):
+		pass
+
+	def onWatchdogFail(self):
+		print 'onWatchdogFail!!!'
+		self.watchdogFailed = True
+
+	def testSetupWatchdog(self):
+		pyWatchdog.setup('watchdog', 0.1, self.onWatchdogFail)
+
+	def testWatchdogTimeout(self):
+		pyWatchdog.setup('watchdog', 0.1, self.onWatchdogFail)
+		self.assertFalse(self.watchdogFailed)
+		self.loopForAWhile(0.11)
+		self.assertTrue(self.watchdogFailed)
+
+	def testWatchdogReset(self):
+		pyWatchdog.setup('watchdog', 0.1, self.onWatchdogFail)
+		self.assertFalse(self.watchdogFailed)
+		self.loopForAWhile(0.05)
+		self.assertFalse(self.watchdogFailed)
+		pyWatchdog.reset('watchdog')
+		self.loopForAWhile(0.05)
+		self.assertFalse(self.watchdogFailed)
+
+	def testWatchdogCancel(self):
+		pyWatchdog.setup('watchdog', 0.1, self.onWatchdogFail)
+		self.assertFalse(self.watchdogFailed)
+		self.loopForAWhile(0.05)
+		self.assertFalse(self.watchdogFailed)
+		pyWatchdog.cancel('watchdog')
+		self.loopForAWhile(0.05)
+		self.assertFalse(self.watchdogFailed)
+
+	def loopForAWhile(self, duration, tickInterval=0.001):
+		endTime = time.time() + duration
+		while time.time() < endTime:
+			time.sleep(tickInterval)
+			pyWatchdog.loop()
 
 if __name__ == '__main__':
 	unittest.main()
